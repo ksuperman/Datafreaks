22bd2195e0912ea248b80dd4b8d2681e81dfa916
Rakshith Koravadi Hatwar <rakshithk@live.com>
2016-11-19 15:27:25 -0800

    Fixed Blank Template

--- pages/blank.php
+++ pages/blank.php
@@ -138,94 +138,10 @@
                             <!-- /input-group -->
                         </li>
                         <li>
-                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i>User</a>
+                            <a href="../index.php"><i class="fa fa-dashboard fa-fw"></i>User</a>
                         </li>
                         <li>
                             <a href="/pages/blank.php"><i class="fa fa-file-o fa-fw"></i>Blank Template</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="pages/flot.html">Flot Charts</a>
-                                </li>
-                                <li>
-                                    <a href="pages/morris.html">Morris.js Charts</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-                        </li>
-                        <li>
-                            <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/panels-wells.html">Panels and Wells</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/buttons.html">Buttons</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/notifications.html">Notifications</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/typography.html">Typography</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/icons.html"> Icons</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/grid.html">Grid</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                    <!-- /.nav-third-level -->
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/blank.html">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="../../Downloads/startbootstrap-sb-admin-2-gh-pages/pages/login.html">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
                         </li>
                     </ul>
                 </div>
